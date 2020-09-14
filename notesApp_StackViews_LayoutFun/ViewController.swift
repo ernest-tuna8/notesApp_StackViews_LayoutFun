ab2b03a2d0f6543aa5db23806aed653c03782ed4
clc babischkin <n/a>
2020-09-14 09:36:01 -0500

    Just got this made

---
+++ notesApp_StackViews_LayoutFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notesApp_StackViews_LayoutFun
+//
+//  Created by Tiger Coder on 9/12/20.
+//  Copyright © 2020 clc babischkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
